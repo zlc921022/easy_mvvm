1b679dc54747bb091e474768e31da06aea516160
zlc921022 <zlc921022@163.com>
2020-04-03 20:18:59 +0800

    修改报错信息

--- app/src/main/java/com/xiaochen/easy_mvvm/MainActivity.kt
+++ app/src/main/java/com/xiaochen/easy_mvvm/MainActivity.kt
@@ -7,20 +7,19 @@ import com.xiaochen.base.utils.LogUtil
 import com.xiaochen.easy_mvvm.viewmodel.TestViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AbsBaseActivity<TestViewModel>() {
+class MainActivity : AbsBaseActivity() {
 
+    private val mViewModel by lazy {
+        ViewModelProviders.of(this).get(TestViewModel::class.java)
+    }
 
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
 
-    override fun createViewModel(): TestViewModel {
-        return ViewModelProviders.of(this).get(TestViewModel::class.java)
-    }
-
     override fun initData() {
         super.initData()
-        mViewModel?.let {
+        mViewModel.let {
             it.mHomeLiveData.observe(this, Observer {
                 LogUtil.e("MainActivity", "调用成功")
             })
@@ -29,7 +28,7 @@ class MainActivity : AbsBaseActivity<TestViewModel>() {
 
     override fun initListener() {
         tv.setOnClickListener {
-            mViewModel?.getArticleInfo(1)
+            mViewModel.getArticleInfo(1)
         }
     }
 }
